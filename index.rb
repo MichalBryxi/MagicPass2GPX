602772738b99ea70c54dd08f97db2efbabd19c48
Michal Bryxí <michal.bryxi@gmail.com>
2023-05-10 10:00:37 +0200

    fix: Searching only for places within CH

--- index.rb
+++ index.rb
@@ -4,27 +4,29 @@ require 'addressable/template'
 require 'json'
 require 'gpx'
 
-LIST_URL = 'https://www.magicpass.ch/en/stations'
-GEOCODE_TEMPLATE = 'https://geocode.maps.co/search/{?query*}'
-OUTPUT_FILE = './Magic Pass.gpx'
+LIST_URL = 'https://www.magicpass.ch/en/stations'.freeze
+GEOCODE_TEMPLATE = 'https://geocode.maps.co/search/{?query*}'.freeze
+OUTPUT_FILE = './Magic Pass.gpx'.freeze
 
+template = Addressable::Template.new(GEOCODE_TEMPLATE)
 list_page = Nokogiri::HTML.parse(URI.open(LIST_URL))
 list_links = list_page.search('.rounded-aio-block.overflow-hidden.group.border.relative')
-places = list_links.map { |item| { name: item['title'], county: item.search('..flex > .flex > .text-gray-500.text-xs.block.break-words').text.trim } }
+places = list_links.map { |item| { name: item['title'], county: item.search('.text-gray-500.text-xs.block.break-words').text.strip } }
 gpx_file = GPX::GPXFile.new
 
 places.each do |place|
-  puts "Processing %s" % [place['name']]
-  template = Addressable::Template.new(GEOCODE_TEMPLATE)
+  puts format('Processing %<name>s', name: place[:name])
+
+  query_string = format('%<county>s, %<name>s, Switzerland', county: place[:county], name: place[:name])
   uri = template.expand({
-    'query' => {
-      'q' => `%s, %s, Switzerland` % [place['county'], place['name']],
+    query: {
+      q: query_string,
     }
   })
   data = JSON.load(URI.open(uri))
   if data[0]
     gpx_file.waypoints << GPX::Waypoint.new({
-      name: place['name'],
+      name: place[:name],
       lat: data[0]['lat'],
       lon: data[0]['lon'],
       desc: data[0]['display_name'],
